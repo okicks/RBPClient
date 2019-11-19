c3df6e28f48842c067c09c672a1b72029e3ffa2e
Oliver <kicks.oliver@gmail.com>
2019-11-18 20:32:49 -0500

    Added some basic stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,42 @@
+/* https://angular.io/guide/http */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatTableModule
+} from '@angular/material';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AgeVerifyComponent } from './components/age-verify/age-verify.component';
+
+const routes = [
+  {path: '**', component: AgeVerifyComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AgeVerifyComponent
   ],
   
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    ReactiveFormsModule
   ],
 
   providers: [],
